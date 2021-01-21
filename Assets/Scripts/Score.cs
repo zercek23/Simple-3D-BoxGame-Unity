69100c36a73dfb4124c09a12b402e9b4fab1f30c
zercek23 <burakbyrm99@gmail.com>
2021-01-21 16:24:53 +0300

    Final

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text ScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tempPosition = -player.position.x;
+        tempPosition = tempPosition + 10;
+        ScoreText.text = tempPosition.ToString("0");
+    }
+}
